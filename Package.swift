641076eea49b3cdb1dfdd5937ef483fa37cb0ab8
Jura Skrlec <jura.skrlec@microblink.com>
2020-11-13 15:59:07 +0100

    Updated initial package
    Updated package framework name
    
    Added package

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "BlinkCard",
+    products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
+        .library(
+            name: "BlinkCard",
+            targets: ["BlinkCard"]),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "BlinkCard",
+            url: "https://github.com/blinkcard/blinkcard-ios/releases/download/v2.0.0/BlinkCard.xcframework.zip",
+            checksum: "")
+    ]
+)
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# blinkcard-swift-package
+# Swift Package for BlinkCard SDK iOS
+
+BlinkCard is a component for quick and easy scanning of payment cards.
+This particular repository provides a swift package for standard BlinkCard SDK which is available as a .xcframework [here](https://github.com/blinkcard/blinkcard-ios).
+
+Please follow the integration and licensing instructions from that repository, and to use swift package manager, refer to section [Swift Package integration](https://github.com/blinkcard/blinkcard-ios#-quick-start).
