fdcc1863ff7a6586bb172d66b9b90a420e83bdc2
Jura Skrlec <jura.skrlec@microblink.com>
2020-11-19 09:39:11 +0100

    Test package

--- Package.swift
+++ Package.swift
@@ -9,13 +9,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "BlinkCard",
-            targets: ["BlinkCard"]),
+            targets: ["Microblink"]),
     ],
     dependencies: [],
     targets: [
         .binaryTarget(
-            name: "BlinkCard",
-            url: "https://github.com/blinkcard/blinkcard-ios/releases/download/v2.0.0/BlinkCard.xcframework.zip",
-            checksum: "")
+            name: "Microblink",
+            url: "https://github.com/blinkcard/blinkcard-ios/releases/download/v1.2.0/Microblink.xcframework.zip",
+            checksum: "98d936c2ddb6b9eca8fd40f6c602c8cb239d23aa5cec8b7b4d00ccc496bcd166")
     ]
 )
