b249de195370d47aa4cbb0967255556951d639b3
Mijo Gracanin <mijo.gracanin@gmail.com>
2023-01-09 17:11:31 +0100

    Update for v2.7.0

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.4
+// swift-tools-version:5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "BlinkCard",
-            url: "https://github.com/blinkcard/blinkcard-ios/releases/download/v2.6.0/BlinkCard.xcframework.zip",
-            checksum: "838137cd7a2cf7fbba0029284d759572a16e37e51bbd9ab9a09ab2703b0ce662")
+            url: "https://github.com/blinkcard/blinkcard-ios/releases/download/v2.7.0/BlinkCard.xcframework.zip",
+            checksum: "d425d2694eec4f7d68f1e7bc547e8cb928b686ac8c0f8e2d9ca2d3469826b701")
     ]
 )
